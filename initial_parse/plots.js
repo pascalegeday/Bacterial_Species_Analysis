32a0d7efbe887e34f7439dbb7bfbfadeb0dbb3dc
Pascale Geday <pascalegeday@Pascales-MacBook-Air.local>
2022-02-15 13:20:46 -0800

    Upload initial data parse

---
+++ initial_parse/plots.js
@@ -0,0 +1,28 @@
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
